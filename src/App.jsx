5eb2843a000f71e5d314701d3f7be1a522f0577a
Rahul Kumar <rahulk8671@gmail.com>
2025-01-25 20:08:55 +0530

    deepseek integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useMemo, useEffect } from 'react';
+import React from 'react'
 import { useSpeechSynthesis } from './hooks/useSpeechSynthesis';
 import { useSpeechRecognition } from './hooks/useSpeechRecognition';
 import microphone from './assets/microphone.svg';
@@ -6,20 +7,20 @@ import speaker from './assets/speaker.svg';
 import loading from './assets/loading.svg';
 import stop from './assets/stop.svg';
 import Gemini from './api/Gemini';
+import DeepSeek from './api/deepseek'
 
 function App() {
-	const gemini = useMemo(() => new Gemini(), []);
-	const gemini1 = useMemo(() => new Gemini(), []);
-	const gemini2 = useMemo(() => new Gemini(), []);
+	const gemini = useMemo(() => new DeepSeek(), []);
+	const gemini1 = useMemo(() => new DeepSeek(), []);
+	const gemini2 = useMemo(() => new DeepSeek(), []);
 	
 	const { speak, isSpeaking, cancelSpeaking } = useSpeechSynthesis();
 	const { speak: speak1, isSpeaking: isSpeaking1, cancelSpeaking: cancelSpeaking1 } = useSpeechSynthesis();
 	const { speak: speak2, isSpeaking: isSpeaking2, cancelSpeaking: cancelSpeaking2 } = useSpeechSynthesis();
 
-	const [history, setHistory] = useState([]);
-
-	const [history1, setHistory1] = useState([]);
-	const [history2, setHistory2] = useState([]);
+	const history = React.useRef([])
+	const history1 = React.useRef([])
+	const history2 = React.useRef([])
 
 	const [isLoading, setIsLoading] = useState(false);
 
@@ -27,18 +28,18 @@ function App() {
 	const [isGemeni2Loading, setIsGemeni2Loading] = useState(false);
 
 	const onRecognitionResult = async (text) => {
-		setHistory((prevHistory) => [...prevHistory, { role: "user", parts: [{ text }] }]);
+		history.current = [...history.current, { role: "user", parts: [{ text }] }]
 
 		setIsLoading(true);
 
-		const responseText = await gemini.sendMessage({ message: text, history: history });
+		const responseText = await gemini.sendMessage({ message: text, history: history.current });
 
 		if (!responseText) {
 			alert('Something went wrong. Please try again.');
 		}
 
 		setIsLoading(false);
-		setHistory((prevHistory) => [...prevHistory, { role: "model", parts: [{ text: responseText }] }]);
+		history.current = [...history.current, { role: "model", parts: [{ text: responseText }] }]
 
 		speak(responseText, () => {
 			startListening();
@@ -47,18 +48,19 @@ function App() {
 
 	const onRecognitionResult1 = async (text) => {
 		console.log('onRecognitionResult1', text);
-		setHistory1((prevHistory) => [...prevHistory, { role: "user", parts: [{ text }] }]);
+		history1.current = [...history1.current, { role: "user", parts: [{ text }] }]
 
 		setIsGemeni1Loading(true);
 
-		const responseText = await gemini1.sendMessage({ message: text, history: history1 });
+		debugger
+		const responseText = await gemini1.sendMessage({ message: text, history: history1.current });
 
 		if (!responseText) {
 			alert('Something went wrong. Please try again.');
 		}
 
 		setIsGemeni1Loading(false);
-		setHistory1((prevHistory) => [...prevHistory, { role: "model", parts: [{ text: responseText }] }]);
+		history1.current = [...history1.current, { role: "model", parts: [{ text: responseText }] }]
 
 		speak1(responseText, () => {
 			console.log('done speaking speak1', responseText);
@@ -68,18 +70,20 @@ function App() {
 
 	const onRecognitionResult2 = async (text) => {
 		console.log('onRecognitionResult2', text);
-		setHistory2((prevHistory) => [...prevHistory, { role: "user", parts: [{ text }] }]);
+		history2.current = [...history2.current, { role: "user", parts: [{ text }] }]
 
 		setIsGemeni2Loading(true);
 
-		const responseText = await gemini2.sendMessage({ message: text, history: history2 });
+		debugger
+		const responseText = await gemini2.sendMessage({ message: text, history: history2.current });
 
 		if (!responseText) {
 			alert('Something went wrong. Please try again.');
 		}
 
 		setIsGemeni2Loading(false);
-		setHistory2((prevHistory) => [...prevHistory, { role: "model", parts: [{ text: responseText }] }]);
+
+		history2.current = [...history2.current, { role: "model", parts: [{ text: responseText }] }]
 
 		speak2(responseText, () => {
 			console.log('done speaking speak2', responseText);
@@ -97,11 +101,11 @@ function App() {
 
 	return (
 		<div style={{ display: 'flex', background: 'gray', height: '100vh', width: '100vw' }}>
-			{/*<div style={{ display: 'flex', borderRight: '5px solid #222831', background: '#31363F', height: '100%', width: '50%', justifyContent: 'center', alignItems: 'center' }}>
+			{/* <div style={{ display: 'flex', borderRight: '5px solid #222831', background: '#31363F', height: '100%', width: '50%', justifyContent: 'center', alignItems: 'center' }}>
 				<div onClick={startListening} style={{ cursor: 'pointer', animation: isListening ? 'expand 0.8s ease-in-out infinite' : 'none', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', background: '#222831', borderRadius: '50%' }}>
 					<img src={microphone} style={{ width: '100px', height: '100px' }} onClick={startListening} />
 				</div>
-			</div>*/}
+			</div> */}
 			<button onClick={() => onRecognitionResult1('Hello')}>Start</button>
 			<div style={{ borderRight: '5px solid #222831', position: 'relative', display: 'flex', background: '#31363F', height: '100%', width: '50%', justifyContent: 'center', alignItems: 'center' }}>
 				<div style={{ animation: isSpeaking1 ? 'expand 0.8s ease-in-out infinite' : 'none', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', background: '#222831', borderRadius: '50%' }}>
---
+++ src/api/deepseek/index.js
@@ -0,0 +1,45 @@
+import { context as defaultContext } from "../Gemini/Context/index.js"
+
+class DeepSeek {
+    constructor(context = false) {
+        this.context = context || defaultContext
+    }
+
+    sendMessage = async ({ message, history }) => {
+        try {
+            // normalize history
+            history = history.map(item => {
+                return {
+                    role: item.role == 'model' ? "assistant" : "user",
+                    content: item.parts[0].text
+                }
+            })
+
+            if (this.context) {
+                history.unshift({
+                    role: 'system',
+                    content: this.context
+                })
+            }
+
+            debugger
+            let response = await fetch(import.meta.env.VITE_DEEP_SEEK_API, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ context: this.context, history }),
+            })
+
+            response = await response.json()
+
+            return response.content
+        } catch (error) {
+            console.error(error)
+
+            return ''
+        }
+    }
+}
+
+export default DeepSeek
