fcff33b49f4c095cecd92ea70262efb0191ab3e0
Rahul Kumar <rahulk8671@gmail.com>
2026-05-17 23:21:32 +0530

    gemini model update

--- src/api/Gemini/index.js
+++ src/api/Gemini/index.js
@@ -6,7 +6,7 @@ class Gemini {
         const genAI = new GoogleGenerativeAI(import.meta.env.VITE_API_KEY);
 
         const model = genAI.getGenerativeModel({
-            model: "gemini-1.5-flash",
+            model: "models/gemini-2.5-flash", 
             systemInstruction: context || defaultContext,
             safetySettings: [
 				{
